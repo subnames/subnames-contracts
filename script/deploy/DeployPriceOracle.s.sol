e5fc250b18ee92e84f11538a74a38059c293d19a
Aki Wu <wuminzhe@gmail.com>
2025-03-07 19:30:10 +0800

    update

--- script/deploy/DeployPriceOracle.s.sol
+++ script/deploy/DeployPriceOracle.s.sol
@@ -12,13 +12,13 @@ contract DeployPriceOracle is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         uint256[] memory prices = new uint256[](6);
-        prices[0] = 316_808_781_402;
-        prices[1] = 31_680_878_140;
-        prices[2] = 3_168_087_814;
-        prices[3] = 316_808_781;
-        prices[4] = 31_680_878;
-        prices[5] = 3_168_087; // 3,168,808.781402895 = 1e14 / (365.25 * 24 * 3600)
-        uint256 premiumStart = 500 ether;
+        prices[0] = 317097919837645900;
+        prices[1] = 31709791983764584;
+        prices[2] = 3170979198376459;
+        prices[3] = 317097919837646;
+        prices[4] = 31709791983765;
+        prices[5] = 3170979198377;
+        uint256 premiumStart = 500000000 ether;
         uint256 totalDays = 28 days;
 
         StablePriceOracle oracle = new ExponentialPremiumPriceOracle(prices, premiumStart, totalDays);
